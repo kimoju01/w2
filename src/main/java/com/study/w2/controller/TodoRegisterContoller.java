0fccea0c5aa812c70cbc303eef6ebbd8aeacbda4
Hyeju <kimoju03@naver.com>
2023-09-11 22:24:57 +0900

    세션을 이용한 로그인 체크
    - https://www.notion.so/Java-6-TIL-2-ef8dc30613914eb1a7b2833d844deee1?pvs=4

--- src/main/java/com/study/w2/controller/TodoRegisterContoller.java
+++ src/main/java/com/study/w2/controller/TodoRegisterContoller.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -24,7 +25,25 @@ public class TodoRegisterContoller extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 입력 화면 보여주기
 
-        log.info("todo register..........");
+        log.info("todo register GET..........");
+
+        HttpSession session = req.getSession();
+
+        // 기존에 JSESSIONID가 없는 새로운 사용자
+        if(session.isNew()) {
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
+            resp.sendRedirect("/login");
+            return; // doGet() 메서드 즉시 종료함 (반환값 없는 return)
+        }
+
+        // JSESSIONID는 있지만 해당 세션 컨텍스트에 loginInfo라는 이름으로 지정된 객체가 없는 경우
+        if(session.getAttribute("loginInfo") == null) {
+            log.info("로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // 정상적인 경우라면 입력 화면으로
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
 
     }
